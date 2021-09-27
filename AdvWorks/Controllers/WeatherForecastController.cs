ebd01912692bd920d7683472b66f4e63d6c459e4
geniedev99 <geniedev99@gmail.com>
2021-09-26 22:34:06 -0400

    added summary

--- AdvWorks/Controllers/WeatherForecastController.cs
+++ AdvWorks/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace AdvWorks.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Sweltering", "Scorching", "cold"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
